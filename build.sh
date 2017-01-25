8dd11fcdf218fe4a69cad7307d251bc5338c3e39
Sal Ferrarello <sal@ironcodestudio.com>
2017-01-25 00:08:38 -0500

    Modify build script to use source and data directories
    See #2
    See #3

--- build.sh
+++ build.sh
@@ -2,28 +2,29 @@
 
 set -ex
 
-curl -L https://wordpress.org/latest.tar.gz -o latest.tgz
-tar zxvf latest.tgz
-rm -fr wordpress/wp-content/
+mkdir -p source
+# curl -L https://wordpress.org/latest.tar.gz -o source/latest.tgz
+tar zxvf source/latest.tgz -C source/
+rm -fr source/wordpress/wp-content/
 
 # action hooks
-find wordpress -name "*.php" | xargs grep "do_action" \
+find source/wordpress -name "*.php" | xargs grep "do_action" \
 | sed "s/^.*do_action/do_action/g"| sed "s/);.*/);/g" \
 | sed "s/ //g" | sed "s/\"/\'/g" | sed "s/do_action(\'//g" \
-| sed "s/\'.*$//g" | sed "s/^.*(.*$//g" | tr -d ' ' | sort | uniq > ./action-hooks.dict
+| sed "s/\'.*$//g" | sed "s/^.*(.*$//g" | tr -d ' ' | sort | uniq > ./data/action-hooks.dict
 
 # filter hooks
-find . -name "*.php" | xargs grep "apply_filters" \
+find source/wordpress -name "*.php" | xargs grep "apply_filters" \
 | sed "s/^.*apply_filters/apply_filters/g"| sed "s/);.*/);/g" \
 | sed "s/ //g" | sed "s/\"/\'/g" | sed "s/apply_filters(\'//g" \
-| sed "s/\'.*$//g" | sed "s/^.*(.*$//g" | tr -d ' ' | sort | uniq > ./filter-hooks.dict
+| sed "s/\'.*$//g" | sed "s/^.*(.*$//g" | tr -d ' ' | sort | uniq > ./data/filter-hooks.dict
 
 # functions
-find . -name "*.php" -exec egrep '^function ' {} \; \
+find source/wordpress -name "*.php" -exec egrep '^function ' {} \; \
 | sed -e "s/function \(.*\)(.*/\1/g" | sed -e "s/^&//g" \
-| sed -e "s/(.*$//g" | tr -d ' ' |sort | uniq > ./functions.dict
+| sed -e "s/(.*$//g" | tr -d ' ' |sort | uniq > ./data/functions.dict
 
 # class
-find . -name "*.php" | xargs grep "^class" \
+find source/wordpress -name "*.php" | xargs grep "^class" \
 | sed "s/.*\:class //g" \
-| sed "s/ .*//g" | tr -d ' ' | sort | uniq > ./classes.dict
+| sed "s/ .*//g" | tr -d ' ' | sort | uniq > ./data/classes.dict
