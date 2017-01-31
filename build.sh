c444678ccc32f7b92aefa0379f71858197cf8b4c
Sal Ferrarello <sal@ironcodestudio.com>
2017-01-31 14:46:27 -0500

    Remove blank line at beginning of dictionary file
    See #7

--- build.sh
+++ build.sh
@@ -11,20 +11,20 @@ rm -fr source/wordpress/wp-content/
 find source/wordpress -name "*.php" | xargs grep "do_action" \
 | sed "s/^.*do_action/do_action/g"| sed "s/);.*/);/g" \
 | sed "s/ //g" | sed "s/\"/\'/g" | sed "s/do_action(\'//g" \
-| sed "s/\'.*$//g" | sed "s/^.*(.*$//g" | tr -d ' ' | sort | uniq > ./data/action-hooks.dict
+| sed "s/\'.*$//g" | sed "s/^.*(.*$//g" | tr -d ' ' | sed "/^$/d" | sort | uniq > ./data/action-hooks.dict
 
 # filter hooks
 find source/wordpress -name "*.php" | xargs grep "apply_filters" \
 | sed "s/^.*apply_filters/apply_filters/g"| sed "s/);.*/);/g" \
 | sed "s/ //g" | sed "s/\"/\'/g" | sed "s/apply_filters(\'//g" \
-| sed "s/\'.*$//g" | sed "s/^.*(.*$//g" | tr -d ' ' | sort | uniq > ./data/filter-hooks.dict
+| sed "s/\'.*$//g" | sed "s/^.*(.*$//g" | tr -d ' ' | sed "/^$/d" | sort | uniq > ./data/filter-hooks.dict
 
 # functions
 find source/wordpress -name "*.php" -exec egrep '^function ' {} \; \
 | sed -e "s/function \(.*\)(.*/\1/g" | sed -e "s/^&//g" \
-| sed -e "s/(.*$//g" | tr -d ' ' |sort | uniq > ./data/functions.dict
+| sed -e "s/(.*$//g" | tr -d ' ' | sed "/^$/d" | sort | uniq > ./data/functions.dict
 
 # class
 find source/wordpress -name "*.php" | xargs grep "^class" \
 | sed "s/.*\:class //g" \
-| sed "s/ .*//g" | tr -d ' ' | sort | uniq > ./data/classes.dict
+| sed "s/ .*//g" | tr -d ' ' | sed "/^$/d" | sort | uniq > ./data/classes.dict
