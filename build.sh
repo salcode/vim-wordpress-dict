51c58cd29469336f4b9c0bcbe9af176f883d6bda
Sal Ferrarello <sal@ironcodestudio.com>
2017-01-31 14:47:24 -0500

    Improve comments in build.sh
    See #5

--- build.sh
+++ build.sh
@@ -7,24 +7,26 @@ curl -L https://wordpress.org/latest.tar.gz -o source/latest.tgz
 tar zxvf source/latest.tgz -C source/
 rm -fr source/wordpress/wp-content/
 
-# action hooks
+# Build dictionary files
+
+## Action hooks dictionary
 find source/wordpress -name "*.php" | xargs grep "do_action" \
 | sed "s/^.*do_action/do_action/g"| sed "s/);.*/);/g" \
 | sed "s/ //g" | sed "s/\"/\'/g" | sed "s/do_action(\'//g" \
 | sed "s/\'.*$//g" | sed "s/^.*(.*$//g" | tr -d ' ' | sed "/^$/d" | sort | uniq > ./data/action-hooks.dict
 
-# filter hooks
+## Filter hooks dictionary
 find source/wordpress -name "*.php" | xargs grep "apply_filters" \
 | sed "s/^.*apply_filters/apply_filters/g"| sed "s/);.*/);/g" \
 | sed "s/ //g" | sed "s/\"/\'/g" | sed "s/apply_filters(\'//g" \
 | sed "s/\'.*$//g" | sed "s/^.*(.*$//g" | tr -d ' ' | sed "/^$/d" | sort | uniq > ./data/filter-hooks.dict
 
-# functions
+## Functions dictionary
 find source/wordpress -name "*.php" -exec egrep '^function ' {} \; \
 | sed -e "s/function \(.*\)(.*/\1/g" | sed -e "s/^&//g" \
 | sed -e "s/(.*$//g" | tr -d ' ' | sed "/^$/d" | sort | uniq > ./data/functions.dict
 
-# class
+## Class dictionary
 find source/wordpress -name "*.php" | xargs grep "^class" \
 | sed "s/.*\:class //g" \
 | sed "s/ .*//g" | tr -d ' ' | sed "/^$/d" | sort | uniq > ./data/classes.dict
